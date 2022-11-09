1780de7d56b5581412cd1353d37d45ebf5dca76d
Campanha <vitorantoniocampanha@gmail.com>
2022-11-09 15:05:52 +0000

    Adding dist

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar";
-import Hero from "./Hero";
-import Services from "./Services";
-import Contact from "./Contact";
-import Maps from "./Maps";
-import Main from "./Main";
-import Footer from "./Footer";
-
-export { Navbar, Hero, Services, Contact, Maps, Main, Footer };
